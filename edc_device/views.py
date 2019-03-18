335c4f4ae0f8f55c9b2809a681565277301bd40e
erikvw <ew2789@gmail.com>
2019-03-18 10:27:46 -0500

    update import for EdcViewMixin

--- edc_device/views.py
+++ edc_device/views.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
-from edc_base.view_mixins import EdcBaseViewMixin
+from edc_dashboard.view_mixins import EdcViewMixin
 from edc_navbar import NavbarViewMixin
 
 from .view_mixins import EdcDeviceViewMixin
 
 
-class HomeView(EdcBaseViewMixin, NavbarViewMixin, EdcDeviceViewMixin, TemplateView):
+class HomeView(EdcViewMixin, NavbarViewMixin, EdcDeviceViewMixin, TemplateView):
 
     template_name = f"edc_device/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
     navbar_name = "edc_device"
@@ -19,7 +19,8 @@ class HomeView(EdcBaseViewMixin, NavbarViewMixin, EdcDeviceViewMixin, TemplateVi
         context = super().get_context_data(**kwargs)
         app_config = django_apps.get_app_config("edc_device")
         project_name = context.get("project_name")
-        context.update({"project_name": f"{project_name}: {app_config.verbose_name}"})
+        context.update(
+            {"project_name": f"{project_name}: {app_config.verbose_name}"})
         return context
 
     @method_decorator(login_required)
