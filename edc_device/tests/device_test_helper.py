e9dd9de44e9236b211e326f1048cca382412f31d
erikvw <ew2789@gmail.com>
2022-08-10 06:17:32 -0500

    precommit and setup

--- edc_device/tests/device_test_helper.py
+++ edc_device/tests/device_test_helper.py
@@ -15,5 +15,8 @@ class DeviceTestHelper:
         app_config.device_id = device_id
         app_config.device_role = device_role
         app_config.ready()
-        assert device_id == app_config.device_id
-        assert device_role == app_config.device_role
+
+        if device_id != app_config.device_id:
+            self.fail("Device mismatch")
+        if device_role != app_config.device_role:
+            self.fail("Role mismatch")
