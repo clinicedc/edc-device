1aa48cf1d518c5f573f3152f5e5acbdf62c497fc
erikvw <ew2789@gmail.com>
2022-10-25 21:30:15 -0500

    add version()

--- edc_device/__init__.py
+++ edc_device/__init__.py
@@ -1,3 +1,7 @@
+from importlib.metadata import version
+
+__version__ = version("edc_device")
+
 from .constants import CENTRAL_SERVER, CLIENT, MIDDLEMAN, NODE_SERVER
 from .device_permission import (
     DeviceAddPermission,
